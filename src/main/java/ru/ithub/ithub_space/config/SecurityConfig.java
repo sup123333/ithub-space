39b6c682083a1135f5f4489ee1b9a8bbacadb8d7
unknown <arnautov.vovan4ik@gmail.com>
2026-06-16 14:32:31 +0300

    pre-site

--- src/main/java/ru/ithub/ithub_space/config/SecurityConfig.java
+++ src/main/java/ru/ithub/ithub_space/config/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig {
             .sessionManagement(session -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/index.html", "/**.css", "/**.js").permitAll()
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/api/portfolios").permitAll()
                 .requestMatchers("/api/events/**").permitAll()
